e27615935df2a556820fca6343b0376ee67f8b39
achesnokov <19andrew02@gmail.com>
2023-02-13 16:18:49 +0200

    3rd commit(dataProvider added to the LoginPageTest)

--- src/test/java/loginPage/tests/LoginPageTest.java
+++ src/test/java/loginPage/tests/LoginPageTest.java
@@ -1,7 +1,6 @@
 package loginPage.tests;
 
 import loginPage.steps.LoginStepsResult;
-<<<<<<< HEAD
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -32,39 +31,6 @@ public class LoginPageTest extends BaseLoginChromeTest {
     @Test(dataProvider = "testInvalidLoginData")
     public void testLoggingWithInvalidData(String username, String password) {
         LoginStepsResult step = steps.loginIntoTheStore(username, password);
-=======
-import org.testng.annotations.Test;
-
-public class LoginPageTest extends BaseLoginChromeTest {
-
-    @Test
-    public void testLoggingOfStandardUser() {
-        LoginStepsResult step = steps.loginIntoTheStore("standard_user", "secret_sauce");
-        step.verifyThatMainPageIsOpened();
-    }
-
-    @Test
-    public  void testLoggingOfLockedOutUser(){
-        LoginStepsResult step = steps.loginIntoTheStore("locked_out_user", "secret_sauce");
-        step.verifyThatValidationMessageIsReceived();
-    }
-
-    @Test
-    public void testLoggingOfProblemUser(){
-        LoginStepsResult step = steps.loginIntoTheStore("problem_user","secret_sauce");
-        step.verifyThatMainPageIsOpened();
-    }
-
-    @Test
-    public void testLoggingOfPerformanceGlitchUser(){
-        LoginStepsResult step = steps.loginIntoTheStore("performance_glitch_user","secret_sauce");
-        step.verifyThatMainPageIsOpened();
-    }
-
-    @Test
-    public void testLoggingOfInvalidUser(){
-        LoginStepsResult step = steps.loginIntoTheStore("123","123");
->>>>>>> github/master
         step.verifyThatValidationMessageIsReceived();
     }
 }
