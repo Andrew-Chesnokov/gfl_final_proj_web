4f0e9052973bc8d46605f9b7cb781e280e1cb00a
achesnokov <19andrew02@gmail.com>
2023-02-17 15:47:36 +0200

    6th (PropertyReader added to utils package)

---
+++ src/test/java/utils/PropertyReader.java
@@ -0,0 +1,30 @@
+package utils;
+
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Properties;
+
+// Singleton pattern
+public class PropertyReader {
+    private static PropertyReader instance = new PropertyReader();
+    private Properties properties;
+
+    @SneakyThrows
+    public PropertyReader() {
+        properties = new Properties();
+        try (BufferedReader reader = new BufferedReader(new FileReader("mytest.properties"))) {
+            properties.load(reader);
+        }
+
+    }
+
+    public static PropertyReader getInstance() {
+        return instance;
+    }
+
+    public String getURL(){
+        return properties.getProperty("url");
+    }
+}
